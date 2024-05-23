fdb0c497f39ba6f31368f431dfb9550571abf1d8
quanganh <ngoquanganh2003a@gmail.com>
2024-05-23 16:11:37 +0700

    update file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UI from './component/UI'
+import ReactBootstrap from './component/ReactBootstrap'
+
+export default function App() {
+  return (
+    <div>
+        App
+        <UI></UI>
+        <ReactBootstrap></ReactBootstrap>
+    </div>
+  )
+}
---
+++ src/component/UI.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function UI() {
+  return (
+    <div>
+        UI  
+        {/* 
+            UI (user interface : giao diện người dùng) là các thư viện UI giúp cho việc tạo giao diện nhanh chóng , dễ dàng hơn 
+            Các thư viện hay dùng :
+                1. react-bootstrap
+                2. Ant-design 
+                3. MUI meterial UI
+                4. tailwind css
+        */}
+    </div>
+  )
+}
